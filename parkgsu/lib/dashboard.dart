ab0d5b9d4c86e2f9dbf85a91f54e02a229144062
Kawsar Ali <143040032+kawsaralii@users.noreply.github.com>
2024-10-23 13:49:56 -0400

    Add files via upload

---
+++ parkgsu/lib/dashboard.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ParkGSU Dashboard'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () {
+              // TODO: Navigate to Profile
+            },
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: _buildDashboardItem(
+                  context, 'Parking Reservation', Icons.local_parking),
+            ),
+            SizedBox(height: 16),
+            Expanded(
+              child: _buildDashboardItem(
+                  context, 'Customer Support', Icons.support),
+            ),
+            SizedBox(height: 16),
+            _buildLogoutButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDashboardItem(
+      BuildContext context, String title, IconData icon) {
+    return Card(
+      elevation: 4.0,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.white,
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(icon, size: 40),
+              SizedBox(height: 8),
+              Text(title,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLogoutButton(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      child: Container(
+        height: 60, // Adjust the height as needed
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.redAccent,
+        ),
+        child: Center(
+          child: Text(
+            'Logout',
+            style: TextStyle(
+                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
