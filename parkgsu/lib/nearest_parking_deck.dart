d2318b3eb2915c0a26949396c1369e467f982303
luvdabhi <25lddabhi@gmail.com>
2024-11-11 12:17:59 -0500

    Added Search by entering location feature, and minor bug fixes

--- parkgsu/lib/nearest_parking_deck.dart
+++ parkgsu/lib/nearest_parking_deck.dart
@@ -3,6 +3,7 @@ import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:geocoding/geocoding.dart';
 import 'reservation.dart';
 
 class NearestParkingDeckScreen extends StatefulWidget {
@@ -21,6 +22,7 @@ class _NearestParkingDeckScreenState extends State<NearestParkingDeckScreen> {
   bool _isLoading = false;
   String _nearestDeckName = '';
   int _openSpots = 0;
+  TextEditingController _searchController = TextEditingController();
 
   @override
   void initState() {
@@ -75,7 +77,33 @@ class _NearestParkingDeckScreenState extends State<NearestParkingDeckScreen> {
     }
   }
 
+  Future<void> _searchLocation(String query) async {
+    try {
+      List<Location> locations = await locationFromAddress(query);
+      if (locations.isNotEmpty) {
+        Location location = locations.first;
+        setState(() {
+          _currentLocation = LatLng(location.latitude, location.longitude);
+          _locationMessage =
+              'Searched location: ${location.latitude}, ${location.longitude}';
+        });
+        _mapController.move(_currentLocation!, 15);
+        _findNearestParkingDeck();
+      } else {
+        setState(() {
+          _locationMessage = 'Location not found';
+        });
+      }
+    } catch (e) {
+      setState(() {
+        _locationMessage = 'Error searching location: $e';
+      });
+    }
+  }
+
   Future<void> _findNearestParkingDeck() async {
+    if (_currentLocation == null) return;
+
     setState(() {
       _isLoading = true;
     });
@@ -115,7 +143,8 @@ class _NearestParkingDeckScreenState extends State<NearestParkingDeckScreen> {
           int openSpots = spotCount - reservedCount;
 
           setState(() {
-            _nearestDeckInfo = 'Parking Deck Name: ${deckData['deck_name'] ?? 'Unknown'}\n'
+            _nearestDeckInfo =
+                'Parking Deck Name: ${deckData['deck_name'] ?? 'Unknown'}\n'
                 'Total Spots: $spotCount\n'
                 'Reserved Spots: $reservedCount\n'
                 'Open Spots: $openSpots';
@@ -150,54 +179,79 @@ class _NearestParkingDeckScreenState extends State<NearestParkingDeckScreen> {
       appBar: AppBar(
         title: const Text(
           'Nearest Parking Deck',
-          style: TextStyle(color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
+          style: TextStyle(
+              color: Colors.white, fontSize: 24, fontWeight: FontWeight.bold),
         ),
         centerTitle: true,
         backgroundColor: Colors.blueAccent[700],
       ),
-      body: Center(
+      body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
+              TextField(
+                controller: _searchController,
+                decoration: InputDecoration(
+                  hintText: 'Enter an address or coordinates',
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none,
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.search, color: Colors.blueAccent),
+                    onPressed: () {
+                      if (_searchController.text.isNotEmpty) {
+                        _searchLocation(_searchController.text);
+                      }
+                    },
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
               Text(
                 _locationMessage,
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 18,
                   color: Colors.white,
                   fontWeight: FontWeight.w500,
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               Text(
                 _nearestDeckInfo.isNotEmpty
                     ? _nearestDeckInfo
                     : "Searching for nearest parking deck...",
-                style: TextStyle(
+                style: const TextStyle(
                   fontSize: 18,
                   color: Colors.white,
                   fontWeight: FontWeight.w500,
                 ),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 20),
+              const SizedBox(height: 20),
               _currentLocation == null
-                  ? CircularProgressIndicator()
-                  : SizedBox(
+                  ? const Center(child: CircularProgressIndicator())
+                  : Container(
                       height: 300,
                       width: double.infinity,
                       child: FlutterMap(
                         mapController: _mapController,
                         options: MapOptions(
                           initialCenter: _currentLocation ?? LatLng(0, 0),
+                          // zoom: 15.0,
                           minZoom: 10.0,
                           maxZoom: 18.0,
                         ),
                         children: [
                           TileLayer(
-                            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                            urlTemplate:
+                                'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                             userAgentPackageName: 'com.example.parkgsu',
                           ),
                           MarkerLayer(
@@ -218,6 +272,7 @@ class _NearestParkingDeckScreenState extends State<NearestParkingDeckScreen> {
                         ],
                       ),
                     ),
+              const SizedBox(height: 20),
             ],
           ),
         ),
@@ -225,26 +280,28 @@ class _NearestParkingDeckScreenState extends State<NearestParkingDeckScreen> {
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ElevatedButton(
-          onPressed: _nearestDeckName.isNotEmpty && _openSpots > 0 && !_isLoading
-              ? () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => ReserveSpotScreen(
-                        selectedDeck: _nearestDeckName,
-                      ),
-                    ),
-                  );
-                }
-              : null,
+          onPressed:
+              _nearestDeckName.isNotEmpty && _openSpots > 0 && !_isLoading
+                  ? () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ReserveSpotScreen(
+                            selectedDeck: _nearestDeckName,
+                          ),
+                        ),
+                      );
+                    }
+                  : null,
           style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.symmetric(vertical: 16),
-            backgroundColor: _openSpots > 0 ? Colors.blueAccent[700] : Colors.grey,
+            padding: const EdgeInsets.symmetric(vertical: 16),
+            backgroundColor:
+                _openSpots > 0 ? Colors.blueAccent[700] : Colors.grey,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(8),
             ),
           ),
-          child: Text(
+          child: const Text(
             'Reserve Spot',
             style: TextStyle(fontSize: 20, color: Colors.white),
           ),
